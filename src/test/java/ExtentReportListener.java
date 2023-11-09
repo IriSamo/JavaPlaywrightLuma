adad5344f5146e18b48db45c7a7a7bf12b9a9cca
IriSamo <i.samosuk@gmail.com>
2023-11-09 15:40:53 -0500

    Add emoji

--- src/test/java/ExtentReportListener.java
+++ src/test/java/ExtentReportListener.java
@@ -17,37 +17,6 @@ import java.util.Date;
 
 public class ExtentReportListener implements ITestListener {
 
-    private static final String OUTPUT_FOLDER = "./extentReport/";
-    private static final String FILE_NAME = "Report";
-
-    private static ExtentReports extent = init();
-    public static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
-    private static ExtentReports extentReports;
-
-    private static ExtentReports init() {
-        Path path = Paths.get(OUTPUT_FOLDER);
-
-        if (!Files.exists(path)) {
-            try {
-                Files.createDirectories(path);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        extentReports = new ExtentReports();
-        ExtentSparkReporter reporter = new ExtentSparkReporter(OUTPUT_FOLDER + FILE_NAME);
-        reporter.config().setReportName("Luma Test Execution Result");
-        extentReports.attachReporter(reporter);
-        extentReports.setSystemInfo("System", "Windows 10");
-        extentReports.setSystemInfo("Author", "IriSamo");
-        extentReports.setSystemInfo("Build", "0.0");
-        extentReports.setSystemInfo("Team", "DT");
-        extentReports.setSystemInfo("Customer Name", "Luma");
-
-        return extentReports;
-    }
-
     @Override
     public synchronized void onStart(ITestContext context) {
         System.out.println("Test Suite started!");
@@ -56,79 +25,20 @@ public class ExtentReportListener implements ITestListener {
     @Override
     public synchronized void onFinish(ITestContext context) {
         System.out.println("Test Suite is ending!");
-        extent.flush();
-        test.remove();
-    }
-
-    @Override
-    public synchronized void onTestStart(ITestResult result) {
-        String methodName = result.getMethod().getMethodName();
-        String qualifiedName = result.getMethod().getQualifiedName();
-        int last = qualifiedName.lastIndexOf(".");
-        int mid = qualifiedName.substring(0, last).lastIndexOf(".");
-        String className = qualifiedName.substring(mid + 1, last);
-
-        System.out.println(methodName + " started!");
-        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName(),
-                result.getMethod().getDescription());
-
-        extentTest.assignCategory(result.getTestContext().getSuite().getName());
-        extentTest.assignCategory(className);
-
-        test.set(extentTest);
-        test.get().getModel().setStartTime(getTime(result.getStartMillis()));
     }
 
     @Override
     public synchronized void onTestSuccess(ITestResult result) {
-        System.out.println(result.getMethod().getMethodName() + " passed!");
-        test.get().pass("Test passed");
-        test.get().pass(result.getThrowable(),
-                MediaEntityBuilder.createScreenCaptureFromPath(takeScreenshot()).build());
-        test.get().getModel().setEndTime(getTime(result.getEndMillis()));
+        System.out.println("\uD83D\uDE42 " + result.getMethod().getMethodName() + " passed!");
     }
 
     @Override
     public synchronized void onTestFailure(ITestResult result) {
-        System.out.println(result.getMethod().getMethodName() + " failed!");
-        test.get().fail("Test failed");
-        test.get().fail(result.getThrowable(),
-                MediaEntityBuilder.createScreenCaptureFromPath(takeScreenshot()).build());
-        test.get().getModel().setEndTime(getTime(result.getEndMillis()));
+        System.out.println("\uD83D\uDC1E " + result.getMethod().getMethodName() + " failed!");
     }
 
     @Override
     public synchronized void onTestSkipped(ITestResult result) {
-        System.out.println(result.getMethod().getMethodName() + " skipped!");
-        test.get().skip("Test skipped");
-        test.get().fail(result.getThrowable(),
-                MediaEntityBuilder.createScreenCaptureFromPath(takeScreenshot()).build());
-        test.get().getModel().setEndTime(getTime(result.getEndMillis()));
-    }
-
-
-    public synchronized void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        System.out.println("onTestFailedButWithinSuccessPercentage for " + result.getMethod().getMethodName());
-        test.get().fail("Test failed");
-        test.get().fail(result.getThrowable(),
-                MediaEntityBuilder.createScreenCaptureFromPath(takeScreenshot()).build());
-        test.get().getModel().setEndTime(getTime(result.getEndMillis()));
-    }
-
-    private Date getTime(long millis) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(millis);
-        return calendar.getTime();
-    }
-
-    public String takeScreenshot() {
-        String path = System.getProperty("user.dir") + "/extentReport/screenshot/" + System.currentTimeMillis() + ".png";
-
-        BaseTest.getPage().screenshot(new Page.ScreenshotOptions()
-                .setPath(Paths.get(path))
-                .setFullPage(true)
-        );
-
-        return path;
+        System.out.println("\uD83D\uDD18 " + result.getMethod().getMethodName() + " skipped!");
     }
 }
